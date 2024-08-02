d4bf4f389bf6b8996271802530bd9d13a9db82e0
Boris Thomas <boris.thomas33@gmail.com>
2024-08-02 09:13:26 +0200

    Refactoring of update_fflib

--- scripts/update_fflib.sh
+++ scripts/update_fflib.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
 
 main(){
-	pull_changes
-	copy_changes_to_directory
-	add_system_to_assert "force-app/fflib/"
-	echo "fflib has been updated."
+    pull_fflib_changes
+    update_fflib_directory
+    add_system_to_assert_in_files "force-app/fflib/"
 }
 
-pull_changes(){
-	echo "Pulling fflib changes..."
-	apex_mocks_output=$(git subtree pull --prefix=fflib-apex-mocks git@github.com:apex-enterprise-patterns/fflib-apex-mocks.git master)
-	apex_common_output=$(git subtree pull --prefix=fflib-apex-common git@github.com:apex-enterprise-patterns/fflib-apex-common.git master)
+pull_fflib_changes(){
+    echo "Pulling fflib changes..."
+    apex_mocks_output=$(git subtree pull --prefix=fflib-apex-mocks git@github.com:apex-enterprise-patterns/fflib-apex-mocks.git master)
+    apex_common_output=$(git subtree pull --prefix=fflib-apex-common git@github.com:apex-enterprise-patterns/fflib-apex-common.git master)
 }
 
-copy_changes_to_directory(){
-	if [[ -n "$apex_mocks_output" ]]; then
-		if [[ $apex_mocks_output = *"Already up to date."* ]]; then
-			echo "No changes on apex mocks."
-		else
-			cp -r fflib-apex-mocks/sfdx-source/apex-mocks/main/* force-app/fflib/
-			cp -r fflib-apex-mocks/sfdx-source/apex-mocks/test/* force-app/fflib/
-		fi
-	fi
-	if [[ -n "$apex_common_output" ]]; then
-		if [[ $apex_common_output = *"Already up to date."* ]]; then
-			echo "No changes on apex common."
-		else
-			cp -r fflib-apex-common/sfdx-source/apex-common/main/* force-app/fflib/
-			cp -r fflib-apex-common/sfdx-source/apex-common/test/* force-app/fflib/
-		fi
-	fi
+update_fflib_directory(){
+    process_subtree_updates "Apex Mocks" "$apex_mocks_output" "fflib-apex-mocks/sfdx-source/apex-mocks" "force-app/fflib/"
+    process_subtree_updates "Apex Common" "$apex_common_output" "fflib-apex-common/sfdx-source/apex-common" "force-app/fflib/"
 }
 
-add_system_to_assert(){
-	if [[ $apex_mocks_output != *"Already up to date."* || $apex_common_output != *"Already up to date."* ]]; then
-		echo "Fixing deploy issue with 'Assert' in files..."
-		for item in "$1"/*; do
-			if [[ -d "$item" ]]; then
-				add_system_to_assert "$item"
-			elif [[ -f "$item" ]]; then
-				sed -i '/System.Assert/!s/\bAssert\b/System.Assert/g' "$item"
-			fi
-		done
-	fi
+process_subtree_updates(){
+    local name="$1"
+    local output="$2"
+    local source_dir="$3"
+    local target_dir="$4"
+
+    if [[ -n "$output" ]]; then
+        if [[ "$output" = *"Already up to date."* ]]; then
+            echo "No changes in $name."
+        else
+            cp -r "$source_dir/main/"* "$target_dir"
+            cp -r "$source_dir/test/"* "$target_dir"
+            echo "$name has been updated."
+        fi
+    fi
+}
+
+add_system_to_assert_in_files(){
+    local dir="$1"
+    if [[ $apex_mocks_output != *"Already up to date."* || $apex_common_output != *"Already up to date."* ]]; then
+        echo -n "Fixing deploy issue with 'Assert' in files... "
+        find "$dir" -type f -exec sed -i '/System.Assert/!s/\bAssert\b/System.Assert/g' {} +
+        echo "Done..."
+    fi
 }
 
 main
