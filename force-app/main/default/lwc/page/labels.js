1f59391a79bbe1aa7039668c2e2c220aae1aa90e
Boris Thomas <boris.thomas33@gmail.com>
2024-10-03 15:14:47 +0200

    Added page lwc

---
+++ force-app/main/default/lwc/page/labels.js
@@ -0,0 +1,9 @@
+import Cancel from '@salesforce/label/c.Cancel';
+import Next from '@salesforce/label/c.Next';
+import Save from '@salesforce/label/c.Save';
+
+export default {
+	Cancel,
+	Next,
+	Save
+};
---
+++ force-app/main/default/lwc/page/page.js
@@ -0,0 +1,96 @@
+import{ LightningElement, api } from 'lwc';
+import{ displaySpinner, hideSpinner } from 'c/utils';
+import label from './labels';
+
+export default class Page extends LightningElement{
+	// Public Properties
+	@api title;
+	@api steps = [];
+	@api nextDisabled = false;
+
+	// Private Properties
+	label = label;
+	scrollingHeight = 0;
+	_step = 0;
+
+	// Getters and Setters
+	get step(){
+		return this._step;
+	}
+
+	@api
+	set step(value){
+		this._step = value;
+	}
+
+	@api
+	get isFirstStep(){
+		return this.step === 0;
+	}
+
+	@api
+	get isSecondStep(){
+		return this.step === 1;
+	}
+
+	@api
+	get isLastStep(){
+		return this.steps.length === this.step;
+	}
+
+	get progressStepLabel(){
+		return this.steps[this.step];
+	}
+
+	get multipleSteps(){
+		return this.steps.length > 1;
+	}
+
+	get nextLabel(){
+		return this.isLastStep ? label.Save : label.Next;
+	}
+
+	// Public Methods
+	@api
+	showSpinner(){
+		displaySpinner(this);
+	}
+
+	@api
+	hideSpinner(){
+		hideSpinner(this);
+	}
+
+	@api
+	incrementStep(){
+		this.step++;
+		this.template.querySelector('.slds-modal__content').scrollTop = 0;
+	}
+
+	// Event Handlers
+	setScrollingHeight(e){
+		this.scrollingHeight = e.target.scrollTop;
+	}
+
+	setStep(e){
+		const newStep = parseInt(e.target.value, 10);
+		if(newStep < this.step){
+			this.step = newStep;
+		}
+	}
+
+	// Dispatch Events
+	dispatchCancel(){
+		this.dispatchEvent(new CustomEvent('cancel'));
+	}
+
+	dispatchNext(){
+		this.dispatchEvent(new CustomEvent('next', {
+			detail: {
+				step: this.step,
+				isFirstStep: this.isFirstStep,
+				isLastStep: this.isLastStep
+			}
+		}));
+	}
+}
