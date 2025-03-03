0793aa65ef4e32fb70b0531f0616ccc79f0a7829
Boris Thomas <boris.thomas33@gmail.com>
2025-03-03 16:23:29 +0100

    Updated chartJs to v4.4.8

--- force-app/unpackaged/lwc/accountModalExample/accountModalExample.js
+++ force-app/unpackaged/lwc/accountModalExample/accountModalExample.js
@@ -22,6 +22,32 @@ export default class AccountModalExample extends LightningElement{
 		];
 	}
 
+	get chartConfig(){
+		return {
+			type: 'polarArea',
+			data: {
+				labels: [
+					'Red',
+					'Green',
+					'Yellow',
+					'Grey',
+					'Blue'
+				],
+				datasets: [{
+					label: 'My First Dataset',
+					data: [11, 16, 7, 3, 14],
+					backgroundColor: [
+					'rgb(255, 99, 132)',
+					'rgb(75, 192, 192)',
+					'rgb(255, 205, 86)',
+					'rgb(201, 203, 207)',
+					'rgb(54, 162, 235)'
+					]
+				}]
+			}
+		};
+	}
+
 	connectedCallback(){
 		setTimeout(() => {
 			hideSpinner(this);
