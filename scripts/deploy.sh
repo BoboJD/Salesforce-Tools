596229383696c833bdc83512358ceb663d84b1be
Boris Thomas <boris.thomas33@gmail.com>
2025-03-21 11:29:45 +0100

    Refactoring of parameters in deploy.sh + added --full deploy option + added ability to add test classes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,47 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 . "$SCRIPT_DIR/utils.sh"
 
 # Parameters (not mandatory)
-# -o / --open-org : navigate to the deploy page of the org
-# -t / --test : execute unit tests while performing deployment
-# -v / --validate : validate the deployment of all source files in a directory to the default org
-# -s / --shutdown : added shutdown option at the end of deployment (error or success of deployment)
-option=$1
+# -f / --full-deploy : perform a full deployment of all source files to the default org
+# -v / --validate : validate the deployment to the default org
+# -t / --test [classNames] : execute unit tests while performing deployment. Optional comma-separated class names
+# -s / --shutdown : shutdown computer at the end of deployment (error or success of deployment)
 
-# Parameters (not mandatory)
-# -s / --shutdown : added shutdown option at the end of deployment (error or success of deployment)
-shutdown=$2
+# Initialize parameters
+full_deploy=false
+validate=false
+run_apex_tests=false
+test_classes=""
+shutdown=false
+
+# Parse command line options
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-f|--full-deploy)
+			full_deploy=true
+			shift
+			;;
+		-v|--validate)
+			validate=true
+			shift
+			;;
+		-t|--test)
+			run_apex_tests=true
+			shift
+			if [[ $# -gt 0 && ! $1 =~ ^- ]]; then
+				test_classes=$1
+				shift
+			fi
+			;;
+		-s|--shutdown)
+			shutdown=true
+			shift
+			;;
+		*)
+			echo "Invalid option: $1" >&2
+			exit 1
+			;;
+	esac
+done
 
 main(){
 	display_start_time
@@ -37,7 +69,7 @@ main(){
 			store_last_deployed_commit_hash
 		fi
 	elif [ "$current_branch" = "prod-release" ]; then
-		if [[ "$is_production_org" = "true" && ("$option" = "-v" || "$option" = "--validate") ]]; then
+		if [[ "$is_production_org" = "true" && "$validate" = true ]]; then
 			error_exit "You need to manually deploy the package in the org to finalize the deployment. Don't forget to update the master branch after."
 		else
 			merge_prod_release_into_master
@@ -48,7 +80,7 @@ main(){
 		reset_tracking_in_scratch_org
 	fi
 	display_duration_of_script
-	if [[ -n "$shutdown" || "$option" = "-s" || "$option" = "--shut-down" ]]; then
+	if [ "$shutdown" = true ]; then
 		cmd.exe /c "shutdown /s /t 0"
 	fi
 }
@@ -95,28 +127,31 @@ edit_files_that_fail_deployment(){
 
 ctrl_c(){
 	echo -ne "\n${RYellow}Aborting deployment... ${NC}"
+	restore_edited_files
 	sf project deploy cancel --use-most-recent > /dev/null
-	echo -e "${RYellow}Done${NC}"
 	exit 1
 }
 
 deploy_files_into_current_org(){
 	get_additional_deploy_parameters
 
-	if [[ "$option" = "-o" || "$option" = "--open-org" ]]; then
-		echo
-		sf org open -p lightning/setup/DeployStatus/home
-	fi
-
 	local type_of_deploy="start"
-	if [[ "$is_production_org" = "true" && ("$option" = "-v" || "$option" = "--validate") ]]; then
+	local start_phrase="Deploying"
+	if [ "$validate" = true ]; then
 		type_of_deploy="validate"
+		start_phrase="Validating"
 	fi
 
-	echo -e "\nDeploying ${RBlue}project metadata files${NC} to org... "
-	local deploy_files_result=$(sf project deploy $type_of_deploy -x manifest/full.xml -w 600 --json$additional_deploy_parameters)
-	local deployment_status=$(echo "$deploy_files_result" | jq -r '.status')
+	if [[ "$is_production_org" = "true" || "$full_deploy" = true ]]; then
+		manifest_file="manifest/full.xml"
+	else
+		construct_deploy_package_xml
+		manifest_file="deployPackage.xml"
+	fi
 
+	echo -e "\n${start_phrase} ${RBlue}project metadata files${NC} to org ${RPurple}${get_org_alias}${NC}... "
+	local deploy_files_result=$(sf project deploy $type_of_deploy -x $manifest_file -w 600 --json$additional_deploy_parameters)
+	local deployment_status=$(echo "$deploy_files_result" | jq -r '.status')
 	if [ "$deployment_status" -eq 0 ]; then
 		echo -e "${RGreen}Deployment successful.${NC}"
 
@@ -124,8 +159,8 @@ deploy_files_into_current_org(){
 			rm "postDeployDestructiveChanges.xml"
 		fi
 
-		if [ "$current_branch" = "preprod" ]; then
-			jq --arg current_commit_hash_or_branch "$current_commit_hash_or_branch" '.lastDeployedCommit = $current_commit_hash_or_branch' "$config_json" > "$config_json.tmp" && mv "$config_json.tmp" "$config_json"
+		if [ "$current_branch" = "preprod" ] && [ -f "$config_file" ] && [ "$(yq eval '.deploy_settings.preprod // "null"' "$config_file")" != "null" ]; then
+			yq eval ".deploy_settings.preprod = \"$current_commit_hash_or_branch\"" -i "$config_file"
 		fi
 	else
 		echo -e "${RRed}Deployment failed. Fix the errors then relaunch the script.${NC}"
@@ -137,7 +172,7 @@ deploy_files_into_current_org(){
 			restore_edited_files
 		fi
 		display_duration_of_script
-		if [[ -n "$shutdown" || "$option" = "-s" || "$option" = "--shut-down" ]]; then
+		if [ "$shutdown" = true ]; then
 			shutdown /s /t 0
 		else
 			local deploy_url=$(echo "$deploy_files_result" | jq -r '.result.deployUrl')
@@ -152,57 +187,56 @@ deploy_files_into_current_org(){
 get_additional_deploy_parameters(){
 	additional_deploy_parameters=""
 
+	if [ "$current_branch" = "preprod" ]; then
+		current_commit_hash_or_branch=$(git rev-parse HEAD)
+		last_deployed_commit=$(yq eval '.deploy_settings.preprod' "$config_file")
+		if [ -n "$last_deployed_commit" ]; then
+			local commit_timestamp=$(git log -1 --format="%ct" $last_deployed_commit)
+			commit_hash_or_branch_reference=$(git log --since="$commit_timestamp" --reverse --format="%H" --first-parent "$current_branch" | awk -v last_commit="$last_deployed_commit" 'BEGIN { found=0; } { if (found) { print $1; exit; } if ($1 == last_commit) found=1; }')
+			if [ -z "$commit_hash_or_branch_reference" ]; then
+				commit_hash_or_branch_reference=$(git log --since="$commit_timestamp" --reverse --format="%H" --first-parent "$current_branch" | awk 'NR==1{print $1; exit}')
+			fi
+			if [[ "$commit_hash_or_branch_reference" = "null" || -z "$commit_hash_or_branch_reference" ]]; then
+				commit_hash_or_branch_reference="master"
+			fi
+			echo -e "\nDeploying into preprod since commit : ${RGreen}${commit_hash_or_branch_reference}${NC}"
+		else
+			commit_hash_or_branch_reference="master"
+			echo -e "\nDeploying into preprod by comparing with master"
+		fi
+	else
+		current_commit_hash_or_branch="$current_branch"
+		commit_hash_or_branch_reference="master"
+	fi
+
 	add_option_to_delete_deleted_or_renamed_files
 
 	if [ "$is_production_org" = "false" ]; then
 		additional_deploy_parameters+=" --ignore-conflicts"
 	fi
 
-	if [[ "$is_production_org" = "true" || "$option" = "-t" || "$option" = "--test" ]]; then
-		additional_deploy_parameters+=" -l RunLocalTests"
+	if [[ "$is_production_org" = "true" || "$run_apex_tests" = true ]]; then
+		if [ -n "$test_classes" ]; then
+			additional_deploy_parameters+=" -t $test_classes"
+		else
+			additional_deploy_parameters+=" -l RunLocalTests"
+		fi
 	fi
 }
 
 add_option_to_delete_deleted_or_renamed_files(){
-	if [ "$current_branch" = "preprod" ]; then
-		config_json=".preprod/config.json"
-		if ! [ -e "$config_json" ]; then
-			error_exit "You need a 'config.json' file in the folder '.preprod/' with a property called 'lastDeployedCommit' that represents the last deployed commit hash in preprod."
-		fi
-		current_commit_hash_or_branch=$(git rev-parse HEAD)
-		local last_deployed_commit=$(jq -r '.lastDeployedCommit' "$config_json")
-
-		local commit_timestamp=$(git log -1 --format="%ct" $last_deployed_commit)
-		commit_hash_or_branch_reference=$(git log --since="$commit_timestamp" --reverse --format="%H" --first-parent "$current_branch" | awk -v last_commit="$last_deployed_commit" 'BEGIN { found=0; } { if (found) { print $1; exit; } if ($1 == last_commit) found=1; }')
-
-		if [ -z "$commit_hash_or_branch_reference" ]; then
-			commit_hash_or_branch_reference=$(git log --since="$commit_timestamp" --reverse --format="%H" --first-parent "$current_branch" | awk 'NR==1{print $1; exit}')
-		fi
-
-		if [[ "$commit_hash_or_branch_reference" = "null" || -z "$commit_hash_or_branch_reference" ]]; then
-			error_exit "In '.preprod/config.json' file, you need a property called 'lastDeployedCommit' with a value that represents the last deployed commit hash in preprod."
-		fi
-
-		echo -e "\nDeploying into preprod since commit : ${RGreen}${commit_hash_or_branch_reference}${NC}"
-	else
-		current_commit_hash_or_branch="$current_branch"
-		commit_hash_or_branch_reference="master"
-	fi
-
 	echo -ne "\nChecking if files have been deleted from project... "
 	local deleted_files=$(git diff --diff-filter=D --name-only $commit_hash_or_branch_reference^ $current_commit_hash_or_branch | awk '$1 ~ /-meta.xml$/ && $1 ~ /^force-app\/main\/default\//')
-	local renamed_files=$(find_renamed_files)
+	local renamed_files=$(find_deleted_files_in_renamed_files)
 	local deleted_labels=$(find_deleted_labels)
 
 	if [[ -n "$deleted_files" || -n "$renamed_files" || -n "$deleted_labels" ]]; then
 		echo -n "Deleted files found."
 		echo -ne "\nGenerating ${RCyan}postDeployDestructiveChanges.xml${NC} to perform deletion on post deployment... "
-		local generated_xml=$(generate_post_deploy_destructive_changes_xml "${deleted_files}\n${renamed_files}\n${deleted_labels}")
-
+		local generated_xml=$(generate_package_xml "${deleted_files}\n${renamed_files}\n${deleted_labels}" true)
 		if grep -q "Metadata type not found" <<< "$generated_xml"; then
-			error_exit "$generated_xml"
+			restore_edited_files_and_exit "$generated_xml"
 		fi
-
 		echo "$generated_xml" > postDeployDestructiveChanges.xml
 		additional_deploy_parameters+=" --ignore-warnings --post-destructive-changes postDeployDestructiveChanges.xml --purge-on-delete"
 		echo "Done."
@@ -211,7 +245,7 @@ add_option_to_delete_deleted_or_renamed_files(){
 	fi
 }
 
-find_renamed_files(){
+find_deleted_files_in_renamed_files(){
 	local renamed_or_moved_files=$(git diff --diff-filter=R --name-status $commit_hash_or_branch_reference^ $current_commit_hash_or_branch | awk '$1 ~ /^R/ && $2 ~ /-meta.xml$/ && $2 ~ /^force-app\/main\/default\// {print $2}')
 
 	local files_to_delete=""
@@ -254,10 +288,24 @@ find_deleted_labels(){
 	echo $labels_to_delete
 }
 
-generate_post_deploy_destructive_changes_xml(){
-	local files_to_delete=$(echo -e $1)
+construct_deploy_package_xml(){
+	local files_to_deploy=$(git diff --diff-filter=ARM --name-only $commit_hash_or_branch_reference^ $current_commit_hash_or_branch | grep -E '^force-app/')
+	if [[ -n "$files_to_deploy" ]]; then
+		echo -ne "\nGenerating ${RCyan}deployPackage.xml${NC} to perform fast deployment..."
+		local generated_xml=$(generate_package_xml "$files_to_deploy" false)
+		if grep -q "Metadata type not found" <<< "$generated_xml"; then
+			restore_edited_files_and_exit "$generated_xml"
+		fi
+		echo "$generated_xml" > deployPackage.xml
+		echo "Done."
+	else
+		restore_edited_files_and_exit "There are no changes to deploy."
+	fi
+}
 
-	files_to_delete=$(echo "$files_to_delete" | tr ' ' '\n')
+generate_package_xml(){
+	local files_to_put_in_xml=$1
+	local is_for_deletion=${2:-false}
 
 	declare -A fileNames_by_metadata_type
 
@@ -265,33 +313,63 @@ generate_post_deploy_destructive_changes_xml(){
 		if [[ $fileFullPath == ${project_directory}* ]]; then
 			local folder=$(echo "$fileFullPath" | awk -F '/' '{print $4}')
 
-			case $folder in
-				"assignmentRules" | "audience" | "autoResponseRules" | "dashboards" | "documents" | "escalationRules" | "experiences" | "managedTopics" | "matchingRules" | "moderation" | "navigationMenus" | "networkBranding" | "networks" | "objectTranslations" | "profiles" | "reports" | "recordTypes" | "reportTypes" | "settings" | "sharingRules" | "siteDotComSites" | "sites" | "standardValueSets" | "territory2Models" | "territory2Types" | "translations" | "userCriteria" | "workflows")
+			if [ "$is_for_deletion" = true ]; then
+				if [[ ! $fileFullPath =~ -meta\.xml$ ]]; then
 					continue
-					;;
-			esac
+				fi
+				case $folder in
+					"assignmentRules" | "audience" | "autoResponseRules" | "dashboards" | "documents" | "escalationRules" | "experiences" | "managedTopics" | "matchingRules" | "moderation" | "navigationMenus" | "networkBranding" | "networks" | "objectTranslations" | "profiles" | "reports" | "recordTypes" | "reportTypes" | "settings" | "sharingRules" | "siteDotComSites" | "sites" | "standardValueSets" | "territory2Models" | "territory2Types" | "translations" | "userCriteria" | "workflows")
+						continue
+						;;
+				esac
+			else
+				case $folder in
+					"connectedApps" | "profiles")
+						continue
+						;;
+				esac
+			fi
 
 			local fileName=$(basename "$fileFullPath" | sed 's/\.[^.]*-meta\.xml$//')
+			if [[ $folder != "quickActions" ]]; then
+				fileName=$(echo "$fileName" | cut -d. -f1)
+			fi
 			if [[ $folder = objects && $fileFullPath != *.object-meta.xml ]]; then
 				local sobject=$(echo "$fileFullPath" | awk -F '/' '{print $5}')
 				sub_folder=$(echo "$fileFullPath" | awk -F '/' '{print $6}')
 				metadata_type=$(find_metadata_type_by_folder_name "$sub_folder")
 				fileName="${sobject}.${fileName}"
+			elif [[ ($folder = aura || $folder = lwc) && $fileFullPath != *.object-meta.xml ]]; then
+				metadata_type=$(find_metadata_type_by_folder_name "$folder")
+				local folderName=$(echo "$fileFullPath" | awk -F '/' '{print $5}')
+				fileName="$folderName"
+			elif [[ $folder = staticresources ]]; then
+				metadata_type=$(find_metadata_type_by_folder_name "$folder")
+				fileName=$(echo "$fileFullPath" | awk -F '/staticresources/' '{print $2}' | cut -d'/' -f1 | cut -d. -f1)
+			elif [[ $folder = email ]]; then
+				if [[ $fileFullPath =~ emailFolder-meta\.xml$ ]]; then
+					metadata_type="EmailFolder"
+				else
+					metadata_type="EmailTemplate"
+					fileName=$(echo "$fileFullPath" | awk -F '/email/' '{print $2}' | cut -d. -f1)
+				fi
 			else
 				metadata_type=$(find_metadata_type_by_folder_name "$folder")
 			fi
 
 			if [ -z "$metadata_type" ]; then
-				error_exit "Metadata type not found for folder '$folder$sub_folder'"
+				metadata_type="Metadata type not found for folder '$folder$sub_folder'"
 			fi
 
 			if [[ "$metadata_type" == "CustomField" && ! "$fileName" =~ __c$ ]]; then
 				continue
 			fi
 
-			fileNames_by_metadata_type["$metadata_type"]+="$fileName "
+			if [[ ! "${fileNames_by_metadata_type[$metadata_type]}" =~ "${fileName};" ]]; then
+				fileNames_by_metadata_type["$metadata_type"]+="$fileName;"
+			fi
 		fi
-	done <<< "$files_to_delete"
+	done <<< "$(echo -e "$files_to_put_in_xml")"
 
 	# Generate XML package
 	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
@@ -299,15 +377,16 @@ generate_post_deploy_destructive_changes_xml(){
 
 	for metadata_type in $(echo "${!fileNames_by_metadata_type[@]}" | tr ' ' '\n' | sort); do
 		echo -e "\t<types>"
-		IFS=' ' read -ra fileNames <<< "${fileNames_by_metadata_type[$metadata_type]}"
-		for fileName in $(echo "${fileNames[@]}" | sort); do
+		IFS=';' read -ra fileNames <<< "${fileNames_by_metadata_type[$metadata_type]}"
+		for fileName in "${fileNames[@]}"; do
 			echo -e "\t\t<members>$fileName</members>"
 		done
 		echo -e "\t\t<name>$metadata_type</name>"
 		echo -e "\t</types>"
 	done
 
-	echo -e "\t<version>63.0</version>"
+	local version=$(jq -r '.sourceApiVersion' sfdx-project.json)
+	echo -e "\t<version>$version</version>"
 	echo "</Package>"
 }
 
@@ -317,28 +396,34 @@ deploy_territories_into_current_org(){
 	echo "Done."
 }
 
-restore_edited_files(){
-	echo -ne "\nResetting changes in files... "
-	local modified_directories=(
-		"connectedApps"
-	)
+restore_edited_files_and_exit(){
+	local error_message=$1;
 	if [ "$is_production_org" = "false" ]; then
-		modified_directories+=("permissionsets")
-		if [ "$is_scratch_org" = "true" ]; then
-			modified_directories+=("queues" "autoResponseRules" "sites")
-		fi
+		restore_edited_files
+	fi
+	error_exit $error_message;
+}
+
+restore_edited_files(){
+	local modified_directories=("connectedApps")
+	if [[ "$is_production_org" = "false" && $(yq eval '.viewallrecords_permissionsets // "null"' "$config_file") != "null" ]]; then
+		while IFS= read -r viewallrecords_permissionset; do
+			git restore "${project_directory}permissionsets/${viewallrecords_permissionset}.permissionset-meta.xml" > /dev/null 2>&1
+		done < <(yq eval '.viewallrecords_permissionsets[]' "$config_file")
+	fi
+	if [ "$is_scratch_org" = "true" ]; then
+		modified_directories+=("queues" "autoResponseRules" "sites")
 	fi
 	for dir in "${modified_directories[@]}"; do
 		git restore "${project_directory}${dir}" > /dev/null 2>&1
 	done
-	echo "Done."
 }
 
 store_last_deployed_commit_hash(){
 	echo -e "\nAdding last deployed commit hash to .preprod/config.json..."
 	yes y | git fetch origin preprod > /dev/null
 	yes y | git pull origin preprod > /dev/null
-	yes y | git add .preprod/config.json > /dev/null
+	yes y | git add .config/salesforce-tools-config.yml > /dev/null
 	yes y | git commit -m "Updated last commit hash deployed in preprod" > /dev/null
 	yes y | git push origin preprod > /dev/null
 }
