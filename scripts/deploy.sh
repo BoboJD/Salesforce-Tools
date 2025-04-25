cd94ea1a920b5a44e7e1adc834bad9c7d8404a54
Boris Thomas <boris.thomas33@gmail.com>
2025-04-25 12:31:36 +0200

    Bypass of territories deploy on quick deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -61,7 +61,7 @@ main(){
 	trap ctrl_c SIGINT
 	deploy_files_into_current_org
 	if [ "$is_production_org" = "false" ]; then
-		if [[ $(yq eval '.org_settings.territories_used // "null"' "$config_file") = "true" ]]; then
+		if [[ "$full_deploy" = true && $(yq eval '.org_settings.territories_used // "null"' "$config_file") = "true" ]]; then
 			deploy_territories_into_current_org
 		fi
 		restore_edited_files
